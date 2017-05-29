654cb854c46617a9f4a59dfdf07e4d830216a2b9
KOHEI IKEDA <godai@techarts.co.jp>
2017-05-30 04:04:10 +0900

    PathFindingのテストを追加

---
+++ proj/libTS/math/pathfinding/test/AstarTest/Assets/PathFinding2D/Resources/Cell.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace PathFinding2D.UI
+{
+    public class Cell : MonoBehaviour
+    {
+        public Text Text;
+        public Image Image;
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+
+}
---
+++ proj/libTS/math/pathfinding/test/AstarTest/Assets/PathFinding2D/Scenes/SceneBehabiour.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneBehabiour : MonoBehaviour {
+
+    public Transform MapRoot;
+    public GameObject CellPrefab;
+    public int GridWidth = 16;
+    public int GridHeight = 16;
+
+    private PathFinding2D.UI.Cell[,] cellMap;
+
+	// Use this for initialization
+	void Start () {
+        this.cellMap = new PathFinding2D.UI.Cell[this.GridHeight, this.GridWidth];
+        for (int y = 0; y < this.GridHeight; ++y)
+        {
+            for (int x = 0; x < this.GridWidth; ++x)
+            {
+                var cell = Instantiate(CellPrefab.gameObject) as GameObject;
+                this.cellMap[x, y] = cell.GetComponent<PathFinding2D.UI.Cell>();
+                cell.transform.SetParent(this.MapRoot, false);
+            }
+
+        }
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

0507e9cea9996833f600ca00401b867463d5d395
KOHEI IKEDA <godai@techarts.co.jp>
2017-06-01 03:01:43 +0900

    A*PathjFindingのサンプル

--- proj/libTS/math/pathfinding/test/AstarTest/Assets/PathFinding2D/Resources/Cell.cs
+++ proj/libTS/math/pathfinding/test/AstarTest/Assets/PathFinding2D/Resources/Cell.cs
@@ -9,17 +9,66 @@ namespace PathFinding2D.UI
     {
         public Text Text;
         public Image Image;
+        public AstarCell AstarCell = new AstarCell();
+        public Color[] CellColor = new Color[5];
 
+        public AstarCell.Type CellType
+        {
+            get { return this.AstarCell.CellType; }
+            set { this.AstarCell.CellType = value; }
+        }
         // Use this for initialization
         void Start()
         {
-
+            var btn = GetComponent<Button>();
+            btn.onClick.AddListener(() => { onClick(); });
         }
 
         // Update is called once per frame
         void Update()
         {
+            this.Image.color = this.CellColor[(int)this.AstarCell.CellType];
+            this.Text.text = string.Format("C:{0:0.00}\nH:{1:0.00}\nS:{2:0.00}\n",
+                this.AstarCell.Cost, this.AstarCell.Hint, this.AstarCell.Score);
+        }
 
+        private void onClick()
+        {
+            if (this.CellType == AstarCell.Type.Block) this.CellType = AstarCell.Type.Empty;
+            else if (this.CellType == AstarCell.Type.Empty) this.CellType = AstarCell.Type.Block;
+        }
+    }
+
+}
+
+namespace PathFinding2D
+{
+    public class AstarCell
+    {
+        public enum Type
+        {
+            Empty,
+            Start,
+            Goal,
+            Open,
+            Close,
+            Block,
+            Correct
+        }
+        public Vector2 Position;
+        public Type CellType = Type.Empty;
+        public float Score = 0.0f;
+        public float Cost = 0.0f;
+        public float Hint = 0.0f;
+        public AstarCell Parent = null;
+
+        public void Reset()
+        {
+            this.CellType = Type.Empty;
+            this.Score = 0.0f;
+            this.Cost = 0.0f;
+            this.Hint = 0.0f;
+            this.Parent = null;
         }
     }
 
--- proj/libTS/math/pathfinding/test/AstarTest/Assets/PathFinding2D/Scenes/SceneBehabiour.cs
+++ proj/libTS/math/pathfinding/test/AstarTest/Assets/PathFinding2D/Scenes/SceneBehabiour.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class SceneBehabiour : MonoBehaviour {
 
@@ -8,27 +9,145 @@ public class SceneBehabiour : MonoBehaviour {
     public GameObject CellPrefab;
     public int GridWidth = 16;
     public int GridHeight = 16;
+    public Vector2 StartPoint = new Vector2(0, 0);
+    public Vector2 GoalPoint = new Vector2(15, 15);
 
-    private PathFinding2D.UI.Cell[,] cellMap;
+    private int StartX { get { return (int)this.StartPoint.x; } }
+    private int StartY { get { return (int)this.StartPoint.y; } }
+    private int GoalX { get { return (int)this.GoalPoint.x; } }
+    private int GoalY { get { return (int)this.GoalPoint.y; } }
+    private bool goled = false;
+    private bool first = true;
 
-	// Use this for initialization
-	void Start () {
-        this.cellMap = new PathFinding2D.UI.Cell[this.GridHeight, this.GridWidth];
+    private PathFinding2D.UI.Cell[] cellMapBody;
+    private PathFinding2D.UI.Cell cellMap(int x, int y)
+    {
+        return cellMapBody[y * this.GridWidth + x];
+    }
+    private void cellMap(int x, int y, PathFinding2D.UI.Cell cell)
+    {
+        cellMapBody[y * this.GridWidth + x] = cell;
+    }
+
+    // Use this for initialization
+    void Start () {
+        this.cellMapBody = new PathFinding2D.UI.Cell[this.GridHeight * this.GridWidth];
         for (int y = 0; y < this.GridHeight; ++y)
         {
             for (int x = 0; x < this.GridWidth; ++x)
             {
                 var cell = Instantiate(CellPrefab.gameObject) as GameObject;
-                this.cellMap[x, y] = cell.GetComponent<PathFinding2D.UI.Cell>();
+                this.cellMap(x, y, cell.GetComponent<PathFinding2D.UI.Cell>());
                 cell.transform.SetParent(this.MapRoot, false);
+                this.cellMap(x, y).AstarCell.Position = new Vector2(x, y);
             }
 
         }
+        this.cellMap(this.StartX, this.StartY).AstarCell.CellType = PathFinding2D.AstarCell.Type.Start;
+        this.cellMap(this.GoalX, this.GoalY).AstarCell.CellType = PathFinding2D.AstarCell.Type.Goal;
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    public void PathFind()
+    {
+        if (this.first)
+        {
+            // スタートマスの回りをスキャン
+            ScanAround(this.cellMap(this.StartX, this.StartY).AstarCell);
+            this.first = false;
+        }
+
+        var cells = this.cellMapBody.Where(c => c.CellType == PathFinding2D.AstarCell.Type.Open)
+                    .OrderBy(c => c.AstarCell.Score)
+                    .ThenBy(c => c.AstarCell.Cost);
+
+        if (cells.Any())
+        {
+            var score = cells.ElementAt(0).AstarCell.Score;
+            foreach(var cell in cells.Where(c => c.AstarCell.Score == score))
+            {
+                ScanAround(cell.AstarCell);
+                if (this.goled) break;
+                cell.AstarCell.CellType = PathFinding2D.AstarCell.Type.Close;
+            }
+        }
+        else
+        {
+            Reset();
+        }
+    }
+
+    private void Goal(PathFinding2D.AstarCell cell)
+    {
+        this.goled = true;
+        var parent = cell;
+        while (parent.Parent != null)
+        {
+            parent.CellType = PathFinding2D.AstarCell.Type.Correct;
+            parent = parent.Parent;
+        }
+    }
+
+    private void ScanAround(PathFinding2D.AstarCell parent)
+    {
+        int x = (int)parent.Position.x;
+        int y = (int)parent.Position.y;
+        for (int dx = -1; dx < 2; ++dx)
+        {
+            for (int dy = -1; dy < 2; ++dy)
+            {
+                if (dx == 0 && dy == 0) continue;
+                int nx = x + dx;
+                int ny = y + dy;
+                if (nx < 0 || nx >= this.GridWidth || ny < 0 || ny >= this.GridHeight) continue;
+                var cell = this.cellMap(nx, ny).AstarCell;
+                if (cell.CellType == PathFinding2D.AstarCell.Type.Goal)
+                {   // !! GOAL!
+                    Goal(parent);
+                    return;
+                }
+
+                Vector2 n = new Vector2(nx, ny);
+                float cost = parent.Cost + Mathf.Sqrt(dx * dx + dy * dy);
+                float hint = (this.GoalPoint - n).magnitude;
+                float score = cost + hint;
+                if (cell.CellType == PathFinding2D.AstarCell.Type.Empty || cell.Score > score)
+                {
+                    cell.Cost = cost;
+                    cell.Hint = hint;
+                    cell.Score = score;
+                    cell.Parent = parent;
+                    cell.CellType = PathFinding2D.AstarCell.Type.Open;
+                }
+            }
+        }
+    }
+    public void Reset()
+    {
+        foreach(var cell in this.cellMapBody)
+        {
+            if (cell.CellType != PathFinding2D.AstarCell.Type.Start && cell.CellType != PathFinding2D.AstarCell.Type.Goal)
+            {
+                cell.AstarCell.Reset();
+            }
+        }
+        this.goled = false;
+        this.first = true;
+    }
+
+    public void OnClickStartButton()
+    {
+        if (this.goled)
+        {
+            Reset();
+        }
+        else
+        {
+            PathFind();
+        }
+    }
 }

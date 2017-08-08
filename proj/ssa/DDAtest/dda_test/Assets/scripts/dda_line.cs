2b6d3d313ad642935d442cd033c8f9efe83ce355
Kohei Ikeda <godai@techarts.co.jp>
2017-08-08 20:15:23 +0900

    コメント追加

--- proj/ssa/DDAtest/dda_test/Assets/scripts/dda_line.cs
+++ proj/ssa/DDAtest/dda_test/Assets/scripts/dda_line.cs
@@ -43,6 +43,42 @@ public class dda_line : MonoBehaviour {
         line(x1,y1,x2, y2, Color.red);
     }
 
+
+    //  線の方程式
+    //  y = a * x + b
+    //  これを微分する
+    //  y' = ?
+    //  デジタル微分では、xが+1したときに、yがどれだけ増えるかを調べる
+    //  y' = (a * (x + 1) + b) - (a * x + b)
+    //  y' = a * x + a - a * x
+    //  y' = a
+    //  つまり、y = a * x + bを微分するとaになる
+    //  xが1増えると、yはaだけ増える 
+    //
+    //  (x1, y1) - (x2, y2) の2点を結ぶ直線を考える
+    //   dx = x2 - x1
+    //   dy = y2 - y1
+    //   a = dy / dx
+    //  
+    //   直線を引くとき、dxとdyの大きさから傾きが45度以上かどうかわかる
+    //   1ドットずつ点を描くために、aの値が1以下になるように計算する
+    //   dx / dy の計算を引き算のループで表現する
+    //   r = dx
+    //   while
+    //   {
+    //     r -= dy
+    //     if (r < 0) r += dx // このとき、yが一つ増える 
+    //   }
+    //   x1とy1を代入して、x2までのループにする
+    //   r = dx
+    //   for (x = x1; x <= x2; ++x)
+    //   {
+    //      r -= dy
+    //      if (r < 0) { r += dx; ++y; }
+    //    }
+    //
+
+
     void line(int x1, int y1, int x2, int y2, Color col)
     {
         int dx = x2 - x1;
@@ -52,7 +88,7 @@ public class dda_line : MonoBehaviour {
 
         if (dx > dy)
         {   // Xの増分が大きいので、Xを1ずつ増やしていく
-            int r = (dx - dy) / 2;
+            int r = dx; //  (dx - dy) / 2;
             int y = y1;
             for (int x = x1; x <= x2; ++x)
             {
